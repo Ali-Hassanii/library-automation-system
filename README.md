b610fb39681c2a63bbb25fae02031b568d7741a2
ali <Ali-A.M.Hassani@outlook.com>
2022-05-14 19:33:44 +0000

    some changes

---
+++ README.md
@@ -0,0 +1,3 @@
+# Python automation library system
+[![Python 3.10](https://img.shields.io/badge/Python-3.10-blue)](https://python.org)
+![CLI](https://img.shields.io/badge/-CLI-yellowgreen)
--- main.py
+++ main.py
@@ -3,28 +3,30 @@ from importlib import import_module
 
 
 def install_prerequisites():
-    print('[!] Some of modules are not installed')
-    install_license = input('Install prerequisites? [Y/N] ')
     while True:
-        if install_license[0] == 'y' or install_license[0] == 'Y':
 
-            modules = ('colorama',)
+        modules = ('colorama',)
 
-            for module in modules:
-                try:
-                    import_module(module)
-                except ModuleNotFoundError:
+        for module in modules:
+            try:
+                import_module(module)
+            except ModuleNotFoundError:
+                install_license = input(
+                    f'[!] `{module}` is not installed\n' +
+                    'Install the module? [Y/N] '
+                )
+                if install_license[0] == 'y' or install_license[0] == 'Y':
                     print(f'Installing {module}, please wait ...\n')
                     print('-' * 15, '***', '-' * 15, '\n')
                     system('pip3 install {0}'.format(module))
-            break
-        else:
-            sure = input('The app might not working! Are you sure? [Y/N] ')
-            if sure[0] == 'y' or sure[0] == 'Y':
-                print('Abort')
-                break
-            else:
-                install_license = 'Y'
+                else:
+                    sure = input('The app might not working! Are you sure? [Y/N] ')
+                    if sure[0] == 'y' or sure[0] == 'Y':
+                        print('Abort')
+                        break
+                    else:
+                        install_license = 'Y'
+        break
 
 
 if __name__ == '__main__':
