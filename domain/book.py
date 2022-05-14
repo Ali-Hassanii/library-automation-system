66dc7e68507c6f7ef02c7f8ee07dda950f09b912
ali <Ali-A.M.Hassani@outlook.com>
2022-05-14 12:58:09 +0000

    Create Book object

---
+++ domain/book.py
@@ -0,0 +1,8 @@
+class Book:
+    def __int__(self, name, code, writer, translator, publisher, genra):
+        self.name = name
+        self.code = code
+        self.writer = writer
+        self.translator = translator
+        self.publisher = publisher
+        self.genra = genra
